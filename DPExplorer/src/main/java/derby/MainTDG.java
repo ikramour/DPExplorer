4aafd12a53282c2b04ea8ade856c0f62a59a0bf8
Youssef OURAHOU <ourahou.y@gmail.com>
2023-12-22 19:48:13 +0100

    commit5

--- DPExplorer/src/main/java/derby/MainTDG.java
+++ DPExplorer/src/main/java/derby/MainTDG.java
@@ -3,20 +3,9 @@ package derby;
 import java.sql.SQLException;
 import java.text.ParseException;
 
-import derby.tdg.BatimentTDG;
-import derby.tdg.CodePostalTDG;
-import derby.tdg.DiagnosticTDG;
-import derby.tdg.RueTDG;
-import derby.tdg.VilleTDG;
 import derby.utils.InserInDbFromCsv;
 
 public class MainTDG {
-
-	public static RueTDG rueTDG;
-	public static CodePostalTDG codePostalTDG;
-	public static VilleTDG villeTDG;
-	public static DiagnosticTDG diagnosticTDG;
-	public static BatimentTDG batimentTDG;
 
 	public static void main(String[] args) throws SQLException, ParseException {
 		new InitDB().initDataBase();
